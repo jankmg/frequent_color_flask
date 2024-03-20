b73ffacfd99feb3566b935734ddd628c23c450b6
jankmg <jankmgdev@gmail.com>
2024-03-20 18:31:22 -0500

    Re: Lambda configuration

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
-import awsgi
 
 #import functions
 from controllers.color.get_dominant_color import get_dominant_color
@@ -16,9 +15,5 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 api.add_resource(welcome, "/")
 api.add_resource(get_dominant_color, "/get_dominant_color")
 
-
-def lambda_handler(event, context):
-    return awsgi.response(app, event, context, base64_content_types={"image/png"})
-
-# if __name__ == '__main__':
-#     app.run(debug=True, port=8080, host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(debug=True, port=8080, host="0.0.0.0")
